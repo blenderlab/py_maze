4d4d404c4e62e03376def999747758cfc0bcc906
blenderlab <thomas@blenderlab.fr>
2022-01-31 11:44:57 +0100

    adding gfx funcitons

--- py_maze_5.py
+++ py_maze_5.py
@@ -14,7 +14,6 @@ NBCELL = 20 #size of the square maze (width)
 maze = [] # contains the area
 visited = [] # contains the visited spots
 backtrack = 0
-vue3D = False
 
 def countn(x,y):
     '''
@@ -57,6 +56,7 @@ def preparerLaby():
     backtrack=0
     maze = viderLaby()
     visited = viderVisited()
+    
 
     # send the 2 arrays : maze & visited 
     return(maze,visited)
@@ -77,6 +77,21 @@ def prochainesCase(x,y):
          if (countn(x+1,y)==3):possibles.append((x+1,y))
     random.shuffle(possibles)
     return (possibles)
+def prochainesCaseSortie(x,y):
+    """
+    returns a collection of tuples (x,y) of available places to go
+    """
+    p = []
+    if (x>1 and maze[x-1][y]==0 and visited[x-1][y]==0 )  :
+        p.append((x-1,y))
+    if (y>1  and maze[x][y-1]==0 and visited[x][y-1]==0) :
+        p.append((x,y-1))
+    if (y<NBCELL  and maze[x][y+1]==0 and visited[x][y+1]==0) :
+        p.append((x,y+1))
+    if (x<NBCELL  and maze[x+1][y]==0 and visited[x+1][y]==0) :
+        p.append((x+1,y))
+    random.shuffle(p)
+    return (p)
 
 def genererLaby(x,y): 
     """
@@ -101,44 +116,57 @@ def genererLaby(x,y):
         return False
     # for each choice...
     for choix in listechoix :
-        afficherLaby(maze,x,y,iso=vue3D)
+        afficherLaby(maze,x,y,)
         #... try to find a way out :
         if genererLaby(choix[0],choix[1]):
             # if it is, adding our point.
             maze[choix[0]][choix[1]]=0
             return True
-    afficherLaby(maze,x,y,iso=vue3D)
+    afficherLaby(maze,x,y,bt=True)
         
     # all the possible ways are wrong : return false...
     # and count backtracking
     backtrack = backtrack +1
-    afficherTexte(maze,22,2,"Backtracking...")
-
     return False
-
-    
-
+def debug(t):
+    for i in range(NBCELL):
+        for j in range(NBCELL):
+            print(t[i][j],end="")
+        print()
+        
 def sortirLaby(x,y):
     global backtrack
     #we are on the last cell (EXIT ! yeaaah!) :
     if x==NBCELL-2 and y==NBCELL-2 :
-         afficherLaby(maze,x,y,player=True,iso=vue3D)
+         afficherLaby(maze,x,y,player=True)
          return True
-    ## Some code to add here ! 
+     # Stipulate that our position is part of the path.
+    visited[x][y]=1
+    # find other possibles ways:
+    listechoix = prochainesCaseSortie(x,y)
+    afficherLaby(maze,x,y,player=True)
+         
+    # if no way out -> backtrack : position is not a path and return false.
+    if len(listechoix)==0:
+        return False
+    # for each choice...
+    for choix in listechoix :
+        #... try to find a way out :
+        if sortirLaby(choix[0],choix[1]):
+            # if it is, adding our point.
+            return True
+            
+    # all the possible ways are wrong : return false...
+    # and count backtracking
+    backtrack = backtrack +1
+    afficherLaby(maze,x,y,player=True,bt=True)
 
-def algoLee(x,y):
-    for i in range(NBCELL):
-        for j in range(NBCELL):
-            if maze[i][j]==0:
-                afficherTexte(maze,i,j,"0",iso=vue3D,color=BLACK)
-
+    return False
 
 #main loop :
 playing = True
 maze,visited = preparerLaby()
-if genererLaby(1,1):
-    afficherTexte(maze,22,2,"Fini")
-
+genererLaby(1,1)
 
 while playing:
     # manage events (keys....)
@@ -156,9 +184,6 @@ while playing:
                 maze,visited = preparerLaby()
                 if genererLaby(1,1):
                     print ("FIni !")
-            if event.key == pygame.K_l:
-                visited = viderVisited()
-                algoLee(1,1)
 
             if event.key == pygame.K_x:
                 visited = viderVisited()
@@ -166,6 +191,8 @@ while playing:
                     print ("Sorti !")
                 else:
                     print ("pas de sortie....")
+
+    
 
    
 pygame.quit()
--- py_maze_gfx.py
+++ py_maze_gfx.py
@@ -17,14 +17,30 @@ img_sand=pygame.image.load("assets/sand.png")
 img_sand=pygame.transform.scale(img_sand, (30,25))
 img_spot=pygame.image.load("assets/spot.png")
 img_spot=pygame.transform.scale(img_spot, (30,25))
+img_back=pygame.image.load("assets/back.png")
+img_back=pygame.transform.scale(img_back, (30,25))
+
+
 # and a little tree 
+
+
 img_tree1=pygame.image.load("assets/tree1.png")
 img_tree1=pygame.transform.scale(img_tree1, (30,40))
 
 img_player=pygame.image.load("assets/player.png")
 img_player=pygame.transform.scale(img_player, (10,20))
 
-    
+WHITE = (255,255,255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+GRAY = (200, 200, 200)
+GRASS= (20,120,20)
+PATH =  (200,200,100)
+SPOTS = (200,0,0)
+PLAYER = (100,200,20)
+
 #Init the pygame engine 
 pygame.init()
 # Screen size :
@@ -37,36 +53,62 @@ clock = pygame.time.Clock()
 
 font = pygame.font.SysFont(None, 24)
 
-def afficherLaby(maze,curx,cury):
+
+def afficherTexte(maze,x,y,texte,iso=False,color=WHITE):
+    cellw = 400//len(maze)
+    img = font.render(texte, True, color)
+    if iso:
+        screen.blit(img,(280+x*(cellw-6)-y*12,40+y*(cellw-14)+x*7))
+    else:
+        screen.blit(img,(x*cellw+cellw/3,y*cellw+cellw/6))
+    pygame.display.flip()
+    
+
+
+def afficherLaby(maze,curx,cury,player=False,iso=False):
     """
     procedure to draw the maze on the screen (isometric view)
     curx & cury is coordinate of current point
     """
     screen.fill((0,0,0)) # Beware : the parameter is a tuple
     # size of a square on the screen : 
-    # Make sure start/end are marked as spot : 
-    maze[1][1]=3
     NBCELL=len(maze)
-    maze[NBCELL-2][NBCELL-2]=3
     # define the cell Width :
     cellw = 400//NBCELL
-    
-    for j in range(0,NBCELL):
-        for i in range(0,NBCELL):
-            # choose the pictcdpure to draw : 
-            if (maze[i][j]==1):
-                screen.blit(img_grass,(280+i*(cellw-6)-j*12,40+j*(cellw-14)+i*7))
-            if (maze[i][j]==0):
-                screen.blit(img_sand,(280+i*(cellw-6)-j*12,40+j*(cellw-14)+i*7))
-            if (maze[i][j]==2):
-                screen.blit(img_grass,(280+i*(cellw-6)-j*12,40+j*(cellw-14)+i*7))
-                screen.blit(img_tree1,(280+i*(cellw-6)-j*12,10+j*(cellw-14)+i*7))
-            if (maze[i][j]==3):
-                screen.blit(img_spot,(280+i*(cellw-6)-j*12,40+j*(cellw-14)+i*7))
-    screen.blit(img_player,(290+curx*(cellw-6)-cury*12,25+cury*(cellw-14)+curx*7))
+    if iso:
+        for j in range(0,NBCELL):
+            for i in range(0,NBCELL):
+                # choose the pictcdpure to draw : 
+                if (maze[i][j]==1):
+                    screen.blit(img_grass,(280+i*(cellw-6)-j*12,40+j*(cellw-14)+i*7))
+                if (maze[i][j]==0):
+                    screen.blit(img_sand,(280+i*(cellw-6)-j*12,40+j*(cellw-14)+i*7))
+                if (i==NBCELL-2 and j==NBCELL-2) or (i==1 and j==1):
+                    screen.blit(img_spot,(280+i*(cellw-6)-j*12,40+j*(cellw-14)+i*7))
+                if curx==i and cury==j:
+                    screen.blit(img_back,(280+curx*(cellw-6)-cury*12,40+cury*(cellw-14)+curx*7))
+       
+        if player:
+            screen.blit(img_player,(290+curx*(cellw-6)-cury*12,25+cury*(cellw-14)+curx*7))
+    if not iso:
+        for j in range(0,NBCELL):
+            for i in range(0,NBCELL):
+                # choose the pictcdpure to draw :
+                rect=pygame.Rect( i*cellw,j*cellw , cellw,cellw)
+                
+                if (maze[i][j]==1):
+                    color=GRASS
+                if (maze[i][j]==0):
+                    color=PATH
+                if (i==NBCELL-2 and j==NBCELL-2) or (i==1 and j==1):
+                    color=SPOTS
+                if curx==i and cury==j:
+                    color=PLAYER
+                pygame.draw.rect(screen,color,rect)
+                
     # update the screen display :   
     pygame.display.flip()
     #some slow down procedure ...
-    time.sleep( 0.1 )
+    time.sleep( 0.01 )
     
     
